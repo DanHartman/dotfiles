48f949012f8a3778f737cfb046ab0610b141bdbe
Dan Hartman <dan.h@rtman.org>
2020-11-19 22:43:59 -0600

    Install xdg-utils

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${0}" )" >/dev/null 2>&1 && pwd )"
 SCRIPT_NAME=$(basename "$0")
 
 # utils
-yay -S curl wget rsync
+yay -S curl wget rsync xdg-utils
 
 # can I get a desktop?
 yay -S i3-wm xorg-xinit rofi
