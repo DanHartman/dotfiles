2cc31ac684cf303966218ffe0da94eb8d9b64ddc
Dan Hartman <dan.h@rtman.org>
2020-12-01 20:05:30 -0600

    Change inline comments to echo commands for rudimentary logging of the process

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,47 +3,47 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${0}" )" >/dev/null 2>&1 && pwd )"
 SCRIPT_NAME=$(basename "$0")
 
-# update system
+echo "update system"
 sudo pacman -Syu
 
-# utils
+echo "utils"
 yay -S curl wget rsync xdg-utils htop openssh
 
-# can I get a desktop?
+echo "can I get a desktop?"
 yay -S i3-wm i3status xorg-xinit rofi xorg-server dmenu
 mkdir -p ${HOME}/Pictures
 curl "https://images.pexels.com/photos/2156/sky-earth-space-working.jpg" > ${HOME}/Pictures/wallpaper.jpg
 
-# fonts
+echo "fonts"
 yay -S ttf-droid ttf-iosevka ttf-font-awesome noto-fonts-emoji
 
-# audio
+echo "audio"
 yay -S pulseaudio pamixer alsa-utils pavucontrol
 
-# video
+echo "video"
 sudo gpasswd -a "${USER}" video
 
-# browsers
+echo "browsers"
 yay -S google-chrome
 sudo xdg-mime default google-chrome-stable.desktop x-scheme-handler/http
 sudo xdg-mime default google-chrome-stable.desktop x-scheme-handler/https
 sudo xdg-mime default google-chrome-stable.desktop text/ht
 
-# shell
+echo "shell"
 yay -S zsh alacritty
 test -d ~/liquidprompt && rm -rf ~/liquidprompt
 cd ~ && git clone https://github.com/nojhan/liquidprompt.git
 
-# chat
+echo "chat"
 yay -S zoom signal-desktop
 
-# dev
+echo "dev"
 yay -S jq go atom docker docker-compose linux-aufs virtualbox virtualbox-host-modules-arch vagrant xclip
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo gpasswd -a "${USER}" docker
 
-# networking
+echo "networking"
 yay -S wpa_supplicant iwlwifi resolvconf openconnect networkmanager-openconnect
 sudo systemctl enable wpa_supplicant
 sudo gpasswd -a "${USER}" network
